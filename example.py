1167af0258ae17120757b2444ef0b17ce384fbec
fefei69 <coolpixs04@gmail.com>
2024-04-20 17:32:12 -0700

    fix warning

--- example.py
+++ example.py
@@ -15,10 +15,10 @@ def example_use_of_gym_env():
     """
 
     print("<========== Example Usages ===========> ")
-    env_path = "./envs/example-8x8.env"
-    # env, info = load_env(env_path) # load an environment
+    env_path = "./starter_code/envs/example-8x8.env"
+    env, info = load_env(env_path) # load an environment
 
-    env, info = load_env("./envs/doorkey-8x8-shortcut.env")
+    # env, info = load_env("starter_code/envs/known_envs/doorkey-8x8-direct.env")
     print("<Environment Info>\n")
     print(info)  # Map size
     # agent initial position & direction,
@@ -29,24 +29,24 @@ def example_use_of_gym_env():
     plot_env(env)
 
     # Get the agent position
-    agent_pos = env.agent_pos
+    agent_pos = env.get_wrapper_attr('agent_pos')
 
     # Get the agent direction
-    agent_dir = env.dir_vec  # or env.agent_dir
+    agent_dir = env.get_wrapper_attr('dir_vec')  # or env.agent_dir 
 
     # Get the cell in front of the agent
-    front_cell = env.front_pos  # == agent_pos + agent_dir
+    front_cell = env.get_wrapper_attr('front_pos')  # == agent_pos + agent_dir 
 
     # Access the cell at coord: (2,3)
-    cell = env.grid.get(2, 3)  # NoneType, Wall, Key, Goal
+    cell = env.get_wrapper_attr('grid').get(2, 3)  # NoneType, Wall, Key, Goal
 
     # Get the door status
-    door = env.grid.get(info["door_pos"][0], info["door_pos"][1])
+    door = env.get_wrapper_attr('grid').get(info["door_pos"][0], info["door_pos"][1])
     is_open = door.is_open
     is_locked = door.is_locked
 
     # Determine whether agent is carrying a key
-    is_carrying = env.carrying is not None
+    is_carrying = env.get_wrapper_attr('carrying') is not None
 
     # Take actions
     cost, done = step(env, MF)  # MF=0, TL=1, TR=2, PK=3, UD=4
@@ -59,12 +59,15 @@ def example_use_of_gym_env():
     print("Picking Up Key Costs: {}".format(cost))
     cost, done = step(env, UD)  # MF=0, TL=1, TR=2, PK=3, UD=4
     print("Unlocking Door Costs: {}".format(cost))
-
+    plot_env(env)
     # Determine whether we stepped into the goal
     if done:
         print("Reached Goal")
 
     # The number of steps so far
-    print("Step Count: {}".format(env.step_count))
+    print("Step Count: {}".format(env.get_wrapper_attr('step_count')))
+
+if __name__ == "__main__":
+    example_use_of_gym_env()
     
 
